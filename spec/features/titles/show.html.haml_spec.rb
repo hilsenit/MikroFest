69bdfc5f11ae545f9c8d47be44de09a5292a5f27
peterhoejlundandersen <hilsenit@gmail.com>
2018-03-04 18:29:53 +0100

    changed link so it works

--- spec/features/titles/show.html.haml_spec.rb
+++ spec/features/titles/show.html.haml_spec.rb
@@ -5,16 +5,16 @@ require 'factories/reviews'
 
 RSpec.feature "Title", type: :feature do
   describe "when showing a title" do
-  let(:publisher) { create(:publisher) }
-  let(:title) { create(:title, publisher_id: publisher.id) }
-  let(:review) { create(:review, title_id: title.id) }
+  let!(:publisher) { create(:publisher) }
+  let!(:title) { create(:title, publisher_id: publisher.id) }
+  let!(:review) { create(:review, title_id: title.id) }
 
-  before { visit publisher_title_path(publisher.id, title.id) }
+  before { visit one_title_path(publisher.id, title.id) }
 
     it "should show reviews if there are any" do
       expect(page).to have_content(review.written_by)
       expect(page).to have_content(review.text)
-      expect(page).to have_content(review.url)
+      expect(page).to have_selector(:css, "[href='#{review.url}']")
     end
 
   end
