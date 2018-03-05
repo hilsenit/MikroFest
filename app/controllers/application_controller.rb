bdce171ad83fdec79498ecf6463ac6f9457b39c7
peterhoejlundandersen <hilsenit@gmail.com>
2018-03-05 21:25:06 +0100

    fixed frontpage after removing skeleton css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    attributes = [:full_name, :address, :email, :password, :password_confirmation, :remember_me]
+    attributes = [:favorite_word, :full_name, :address, :email, :password, :password_confirmation, :remember_me]
     devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
     devise_parameter_sanitizer.permit(:account_update, keys: attributes)
   end
