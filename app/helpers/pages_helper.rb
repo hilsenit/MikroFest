a2d271daf061982b327b599c4729dae4e93412de
peterhoejlundandersen <hilsenit@gmail.com>
2018-03-05 11:36:34 +0100

    started on user profile helper so it's possible to visit each site from an url

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,16 @@
 module PagesHelper
+  def tabs_open_helper tab_param
+    case tab_params
+    when "user-information"
+      "active"
+    when "favorites"
+      "active"
+    when "purchases"
+      "active"
+    when "contact"
+      "active"
+    else
+      ""
+    end
+  end
 end
