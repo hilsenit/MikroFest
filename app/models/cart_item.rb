d5a7aab6e2dca52784131926c546ac5f6b81fccc
peterhoejlundandersen <hilsenit@gmail.com>
2018-04-23 19:34:27 +0200

    cart designed

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,10 @@
 class CartItem < ApplicationRecord
+  attr_accessor :titles_price
+
   belongs_to :cart
   belongs_to :title
+
+  def get_titles_price
+    self.titles_price = quantity * title.price
+  end
 end
